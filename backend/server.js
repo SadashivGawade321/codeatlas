0232cd6d5bf8ce0683fbf9a38d8af86a8ff17107
Sadashiv <gawadelaxmi075@gamil.com>
2026-06-15 23:24:12 +0530

    Add Netlify redirects

--- backend/server.js
+++ backend/server.js
@@ -103,7 +103,7 @@ io.on('connection', (socket) => {
 });
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/codeatlas')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
